ab6259b7cb2eac66d674501c456fc32a539a306f
gorz <c.rodrigoalvz@gmail.com>
2024-07-22 02:01:30 -0300

    add Pencil, Eraser, Rainbow, Progresive & Clear tools

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const gridContainer = document.getElementById("grid-container");
+const cells = document.querySelectorAll('.cell')
+
+let tool = 'pencil'
+let color = 'black'
+let opacity = 0.0
 
 const generateGrid = (size) => {
   const itemSize = `calc(100% / ${size})`;
@@ -20,6 +25,42 @@ const deleteGrid = (container) => {
 const updateGrid = (container, size) => {
   deleteGrid(container);
   generateGrid(size);
+  attachEventListeners()
 };
 
+const attachEventListeners = () => {
+  
+  const cells = document.querySelectorAll('.cell');
+  cells.forEach(cell => {
+    cell.addEventListener('mouseover', () => {
+      if (tool === 'pencil') {
+        cell.style.backgroundColor = color;
+      }else if (tool === 'erase') {
+        cell.style.backgroundColor = 'white'
+      }else if(tool === 'rainbow') {
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+
+        cell.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+      }else if (tool === 'progresive'){
+        
+        if (opacity !== 1.0) {
+          opacity += 0.1
+        }
+        cell.style.backgroundColor = color
+        cell.style.opacity = opacity
+      }
+    });
+  });
+};
+
+const clear = () => {
+  const cells = document.querySelectorAll('.cell');
+  cells.forEach(cell => {
+    cell.style.backgroundColor = 'white'
+  })
+}
+
 generateGrid(32);
+attachEventListeners()
